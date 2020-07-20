96f0d43e5bcc71f5a1f823be06e03fbb411ac785
Joao Acharán Riffo <joaoacharan@gmail.com>
2020-07-20 15:04:40 -0400

    CORRECCION con case_when

--- code/code_01_setup_02_malla_validacion.R
+++ code/code_01_setup_02_malla_validacion.R
@@ -28,29 +28,29 @@ reporte_errormissing <-  reporte_errormissing %>% filter(variable != "id_viviend
 data_errorpro <- data_enut      %>% select(id_vivienda,id_hogar,id_persona)
 
 ##ERRORPRO_1: Responde que la semana pasada si trabajó personal servicio domestico y luego no responde cuantas personas
-data_errorpro   <- data_errorpro %>% add_column(ERRORPRO_1  = data_enut %>% transmute(errorpro = xor(f11_1_1 == 1, !is.na(f12_1_1))))
-data_ERRORPRO_1 <- data_errorpro %>% filter(ERRORPRO_1 == TRUE)
+data_errorpro   <- data_errorpro %>% add_column(ERRORPRO_1  = data_enut %>% transmute(errorpro = case_when(f11_1_1 == 1 & is.na(f12_1_1) ~ 1, TRUE ~ 0)))
+data_ERRORPRO_1 <- data_errorpro %>% filter(ERRORPRO_1 == 1)
 data_ERRORPRO_1 <- left_join(data_ERRORPRO_1,data_enut, by = c("id_vivienda", "id_hogar", "id_persona"))
 data_ERRORPRO_1 <- data_ERRORPRO_1 %>% select(id_vivienda,id_hogar,id_persona,ERRORPRO_1,f11_1_1,f12_1_1)
 data_ERRORPRO_1
 
 ##ERRORPRO_2: Responde que la semana pasada NO trabajó personal servicio domestico y luego responde cuantas personas trabajaron
-data_errorpro   <- data_errorpro %>% add_column(ERRORPRO_2  = data_enut %>% transmute(errorpro = xor(f11_1_1 == 2, is.na(f12_1_1))))
-data_ERRORPRO_2 <- data_errorpro %>% filter(ERRORPRO_2 == TRUE)
+data_errorpro   <- data_errorpro %>% add_column(ERRORPRO_2  = data_enut %>% transmute(errorpro = case_when(f11_1_1 == 2 & !is.na(f12_1_1) ~ 1, TRUE ~ 0)))
+data_ERRORPRO_2 <- data_errorpro %>% filter(ERRORPRO_2 == 1)
 data_ERRORPRO_2 <- left_join(data_ERRORPRO_2,data_enut, by = c("id_vivienda", "id_hogar", "id_persona"))
 data_ERRORPRO_2 <- data_ERRORPRO_2 %>% select(id_vivienda,id_hogar,id_persona,ERRORPRO_2,f11_1_1,f12_1_1)
 data_ERRORPRO_2
 
 ##ERRORPRO_3: Responde que la semana pasada si trabajó personal servicio domestico y luego no responde sexo de personal domestico
-data_errorpro   <- data_errorpro %>% add_column(ERRORPRO_3  = data_enut %>% transmute(errorpro = xor(f11_1_1 == 1, !is.na(f13_1_1))))
-data_ERRORPRO_3 <- data_errorpro %>% filter(ERRORPRO_3 == TRUE)
+data_errorpro   <- data_errorpro %>% add_column(ERRORPRO_3  = data_enut %>% transmute(errorpro = case_when(f11_1_1 == 1 & is.na(f13_1_1) ~ 1, TRUE ~ 0)))
+data_ERRORPRO_3 <- data_errorpro %>% filter(ERRORPRO_3 == 1)
 data_ERRORPRO_3 <- left_join(data_ERRORPRO_3,data_enut, by = c("id_vivienda", "id_hogar", "id_persona"))
 data_ERRORPRO_3 <- data_ERRORPRO_3 %>% select(id_vivienda,id_hogar,id_persona,ERRORPRO_3,f11_1_1,f13_1_1)
 data_ERRORPRO_3
 
 ##ERRORPRO_4: Responde que la semana pasada NO trabajó personal servicio domestico y luego responde sexo de personal domestico
-data_errorpro   <- data_errorpro %>% add_column(ERRORPRO_4  = data_enut %>% transmute(errorpro = xor(f11_1_1 == 2, is.na(f13_1_1))))
-data_ERRORPRO_4 <- data_errorpro %>% filter(ERRORPRO_4 == TRUE)
+data_errorpro   <- data_errorpro %>% add_column(ERRORPRO_4  = data_enut %>% transmute(errorpro = case_when(f11_1_1 == 2 & !is.na(f13_1_1) ~ 1, TRUE ~ 0)))
+data_ERRORPRO_4 <- data_errorpro %>% filter(ERRORPRO_4 == 1)
 data_ERRORPRO_4 <- left_join(data_ERRORPRO_4,data_enut, by = c("id_vivienda", "id_hogar", "id_persona"))
 data_ERRORPRO_4 <- data_ERRORPRO_4 %>% select(id_vivienda,id_hogar,id_persona,ERRORPRO_4,f11_1_1,f13_1_1)
 data_ERRORPRO_4
@@ -65,7 +65,7 @@ reporte_errorpro <-  reporte_errorpro %>% filter(variable != "id_vivienda" & var
 data_errorcon <- data_enut      %>% select(id_vivienda,id_hogar,id_persona)
 
 ##ERRORCON_1: Responde que tiene estudios de magister o doctorado y edad inferior a 21 años
-data_errorcon   <- data_errorcon %>% add_column(ERRORCON_1 = data_enut %>% transmute(errorcon = xor(c14_1_1 <= 21, d12_1_1 == 14 | d12_1_1 == 15)))
+data_errorcon   <- data_errorcon %>% add_column(ERRORCON_1 = data_enut %>% transmute(errorcon = case_when(c14_1_1 <= 21 & (d12_1_1 == 14 | d12_1_1 == 15) ~ 1, TRUE ~ 0)))
 data_errorcon_1 <- data_errorcon %>% filter(ERRORCON_1 == TRUE)
 data_errorcon_1 <- left_join(data_errorcon_1,data_enut, by = c("id_vivienda", "id_hogar", "id_persona"))
 data_errorcon_1 <- data_errorcon_1 %>% select(id_vivienda,id_hogar,id_persona,ERRORCON_1,f11_1_1,f13_1_1)
